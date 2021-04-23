48fec5c106c3472e61625615806f7cdff051b16f
beingbravado <sharmagyanesh199@gmail.com>
2021-04-23 13:40:54 +0530

    Updates

---
+++ precache-manifest.a809d7303a0ceaae186d8fcb84e16546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b657d9b151aaeede1104ad27bd33f52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdfbd98a947b5801b4da",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7416ca9ee8afef744913",
+    "url": "/static/js/2.a7cc0c3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a7cc0c3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdfbd98a947b5801b4da",
+    "url": "/static/js/main.b620f9ee.chunk.js"
+  },
+  {
+    "revision": "6f66a38a23320cf835ab",
+    "url": "/static/js/runtime-main.1b2b1293.js"
+  }
+]);
